0481d20d38375f8231934a6502d9e1b1cb81b57c
Sandi Metz <sandi@sandimetz.com>
2018-08-02 14:40:06 -0700

    House before composition

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -12,20 +12,25 @@ class House
       "the rat that ate",
       "the malt that lay in",
       "the house that Jack built"]
+  attr_reader :data
+
+  def initialize
+    @data = DATA
+  end
 
   def recite
     1.upto(12).collect {|i| line(i)}.join("\n")
   end
 
-  def phrase(num=1)
+  def phrase(num)
     data.last(num).join(" ")
   end
 
   def line(num)
-    "This is #{phrase(num)}.\n"
+    "#{prefix} #{phrase(num)}.\n"
   end
 
-  def data
-    DATA
+  def prefix
+    "This is"
   end
 end
